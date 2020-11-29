879ef517a8449a134bbf8b9a4a9b80c7b190972a
fletcher3 <jan.tiegges@campus.tu-berlin.de>
2020-11-29 23:36:36 +0100

    TODO: handle null-bytes, send random line

--- Block2/server.c
+++ Block2/server.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
 #include <signal.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <time.h>
 
-#define BACKLOG 1   // how many pending connections queue will hold
-#define MAXDATASIZE 512 // max number of bytes we send at once
+#define BACKLOG 1
 
-#define MAX_NUM_QUOTES 50
-#define MAX_QUOTELENGTH 512
 
-int main(int argc, char *argv[])
+void sigchld_handler(int s)
 {
-    // declare variables
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-    struct addrinfo hints, *res, *p;
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size;
-    int yes=1;
-    int status;
+    int saved_errno = errno;
 
-    FILE *fptr;
-    int line_counter = 0;
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+    errno = saved_errno;
+}
 
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+int main (int argc, char *argv[]) {
+    //read qotd.txt
+
+    char *line_buf = NULL;
+    size_t line_buf_size = 0;
+    int line_count = 0;
+    ssize_t line_size;
+    FILE *fp;
 
     // check if enough arguments were given
     if (argc != 3) {
@@ -37,42 +45,50 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // TODO
-    // check if port number is in range
-    //int port_int = atoi(argv[1]);
+    fp = fopen(argv[2], "r");
+    if (fp==NULL)
+    {
+        fprintf(stderr, "Error opening file '%s'\n", argv[2]);
+        return EXIT_FAILURE;
+    }
 
-    // open file in read mode and check for error
-    //fptr = fopen(argv[2], "r");
-    fptr = fopen("../qotd.txt", "r");
-
-    if(fptr == NULL){
-        perror("Server - File did not open: ");
-        exit(1);
+    // get number of lines in file
+    line_size = getline(&line_buf, &line_buf_size, fp);
+    while (line_size >= 0)
+    {
+        line_count++;
+        line_size = getline(&line_buf, &line_buf_size, fp);
     }
 
 
-    // set parameters for addrinfo; works with IPv4 and IPv6; Stream socket
+    //
+    int sockfd, new_fd;
+    int yes = 1;
+    struct addrinfo hints, *servinfo, *p;
+    struct sigaction sa;
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
+    hints.ai_flags = AI_PASSIVE;
 
-    // get addrinfo
-    if ((status = getaddrinfo(NULL, argv[1], &hints, &res)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+
+    if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
-    // loop through all the results and bind to the first we can
-    for(p = res; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                             p->ai_protocol)) == -1) {
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("server: socket");
             continue;
         }
 
-        if ((status = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                       sizeof(int))) == -1) {
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
             perror("setsockopt");
             exit(1);
         }
@@ -82,68 +98,81 @@ int main(int argc, char *argv[])
             perror("server: bind");
             continue;
         }
+
         break;
     }
 
-    freeaddrinfo(res); // all done with this structure
+    freeaddrinfo(servinfo);
 
-    // print error if binding failed
-    if (p == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
+    if (p == NULL) {
+        fprintf(stderr, "server: failed do bind\n");
         exit(1);
     }
 
-    // listen
-    if ((status =listen(sockfd, BACKLOG)) == -1) {
+    if (listen(sockfd, BACKLOG) == -1) {
         perror("listen");
+        exit(1);
+    }
+
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
         exit(1);
     }
 
     printf("server: waiting for connections...\n");
 
-    //Get number of lines in document
-    char c;
-    for (c = getc(fptr); c != EOF; c = getc(fptr)){
-        if (c == '\n') // Increment count if this character is newline
-            line_counter++;
-    }
-
-    char quotes[MAX_NUM_QUOTES][MAX_QUOTELENGTH];
-    int quote_length[MAX_QUOTELENGTH];
-
-    int i = 0;
-    int j = 0;
-
-    // saves quotes and length of quotes in array
-    while(fgets(quotes[i], MAX_QUOTELENGTH, fptr)){
-        while(j<MAX_QUOTELENGTH && quotes[i][j] != "\n"){
-            j++;
-        }
-        quote_length[i] = j;
-        i++;
-    }
-
-    line_counter = i;
-
-
-    // always stay ready for connection
-    while(1) {
-        // creates a new socket for connecting client
+    while (1) {
         sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size);
         if (new_fd == -1) {
             perror("accept");
             continue;
         }
 
-        // create random number of a line in document
-        int rand_line = rand() % line_counter;
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *) &their_addr), s, sizeof s);
+        printf("server: got connection from %s\n", s);
 
-        if ((status = send(new_fd, quotes[rand_line], quote_length[rand_line], 0)) == -1)
-            perror("send");
+        fclose(fp);
+        fp = fopen(argv[2], "r");
 
+        if (!fork()) {
+            close(sockfd);
+            // takes random line
+            time_t t;
+            t = (unsigned) time(&t);
+            srand(t);
+            int r = rand() % line_count;
+
+            // sends random line
+            //TODO how to send random line
+            //for(int i=0;i<r;i++) {
+            line_buf_size = 0;
+            line_buf = NULL;
+            ssize_t tem = getline(&line_buf, &line_buf_size, fp);
+            if (tem == -1) {
+                perror("Server - Getline failed: ");
+                exit(1);
+            }
+            //TODO How to handle strings with null-bytes bc strlen leaves them out
+            int i;
+            char quote[line_buf_size];
+            int quote_length = 0;
+            for (i = 0; i < strlen(line_buf); i++){
+                if(line_buf[i] != '\n'){
+                    quote[quote_length] = line_buf[i];
+                    quote_length++;
+                }
+            }
+            if (send(new_fd, quote, quote_length, 0) == -1)
+                perror("send");
+            close(new_fd);
+            exit(0);
+            //}
+        }
         close(new_fd);
-        exit(0);
 
     }
     return 0;
